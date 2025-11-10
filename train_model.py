f211a154c5aee3ce30e4847240eab79f9761b18f
SB <136708729+Shameerburney@users.noreply.github.com>
2025-11-10 16:52:40 +0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import LinearRegression
 import pickle
 
-# 🧠 Example dataset: [humidity, wind_speed] -> temperature
+# Small dataset: [humidity, wind_speed] -> temperature
 X = [
     [30, 2],
     [45, 3],
@@ -14,14 +14,11 @@ X = [
     [70, 4.8],
     [65, 3.1]
 ]
-
 y = [35, 32, 28, 26, 25, 24, 30, 33, 27, 29]
 
-# 🔧 Train model
 model = LinearRegression()
 model.fit(X, y)
 
-# 💾 Save model to file
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
 
