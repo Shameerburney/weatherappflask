1ad067fe53a64ace5ac92c01d26638a81ddf347c
SB <136708729+Shameerburney@users.noreply.github.com>
2025-11-10 16:41:06 +0500

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 
-# 🧠 Load trained model
+# 🧠 Load the trained model
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
